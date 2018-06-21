8c3c2a59aa7dfa20c54dbb6b6c4e39d991c359ec
parrytsj <parry.tsj@gmail.com>
2018-06-21 22:54:24 +0600

    21/05/2018 - Working segue. Three ViewControllers

---
+++ Case/TouchIDViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TouchIDViewController.swift
+//  Case
+//
+//  Created by Thomas Parry on 21/06/2018.
+//  Copyright © 2018 Thomas Parry. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class TouchIDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func showAlertController(_messsage:String) {
+        let alertController = UIAlertController(title: nil, message: "message", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
--- Case/ViewController.swift
+++ Case/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var RegisterButton: UIButton!
+    
+    @IBAction func showMessage(sender: UIButton) {
+        switch sender {
+        case LoginButton:
+            let LoginButton = UIAlertController(title: "Log In:", message: "This will link to TouchID page", preferredStyle: UIAlertControllerStyle.alert)
+            LoginButton.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default))
+            present(LoginButton, animated: true, completion: nil)
+            
+        default:
+            break
+        }
 }
 
+}
